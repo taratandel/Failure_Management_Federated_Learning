83f295f11fcca386ca989be1740b389e8f97cda7
Tara <tara.tandel@icloud.com>
2021-03-16 15:36:47 +0100

    Add coordinator

---
+++ venv/Coordinator.py
@@ -0,0 +1,23 @@
+class coordinator:
+    clients =[Client]
+
+    def initializeWeight(self):
+        weights = initial_weights
+    return weights
+
+    def pickTheClients(self):
+        clients = clients
+    return clients
+
+    def aggregateTheRecievedModels(self):
+        agg_weights = weights
+
+    return agg_weights
+
+    def checkForConvergence(self):
+
+    return true
+
+    def broadCast(self):
+
+    return weights
---
+++ venv/FedAVG.py
@@ -0,0 +1,15 @@
+from Coordinator import *
+
+coordinator = Coordinator()
+# client register
+t = 10
+for i in range(t)
+coordinator.initializeWeight()
+coordinator.pickTheClients()
+# for client in selected client
+#   start training
+#   return the trained weight
+
+coordinator.aggregateTheRecievedModels()
+coordinator.checkForConvergence()
+coordinator.broadCast()
